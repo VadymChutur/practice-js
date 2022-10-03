abe35fb5d9c62aa3474c0e98b9f73f3ae9b032d5
Vadym Chutur <falloutgreck@gmail.com>
2022-10-03 12:36:03 +0300

    update

--- js/module-9.js
+++ js/module-9.js
@@ -446,3 +446,36 @@
 // }
 
 // console.log(cutWords(input));
+const STORAGE_KEY = 'feedback-msg';
+
+const refs = {
+  form: document.querySelector('.js-feedback-form'),
+  textarea: document.querySelector('.js-feedback-form textarea'),
+};
+
+refs.form.addEventListener('submit', onFormSubmit);
+refs.textarea.addEventListener('input', onTextareaInput);
+
+populateMessageOutput();
+
+function onTextareaInput(evt) {
+  const message = evt.target.value;
+
+  localStorage.setItem(STORAGE_KEY, message);
+}
+
+function onFormSubmit(evt) {
+  evt.preventDefault();
+
+  console.log('send form');
+  evt.currentTarget.reset();
+  localStorage.removeItem(STORAGE_KEY);
+}
+
+function populateMessageOutput() {
+  const savedMessage = localStorage.getItem(STORAGE_KEY);
+  if (savedMessage) {
+    console.log(savedMessage);
+    refs.textarea.value = savedMessage;
+  }
+}
